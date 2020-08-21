f4a897e14935863a3808023f30458f46505f1125
Claudio Santos das Virgens <csantos@MB-Pro-de-CSV.local>
2020-08-21 17:38:46 -0300

    loading component, menu Component, Menu Styling.

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -9,7 +9,8 @@ class Signin extends Component {
             email: "",
             password: "",
             error: "",
-            redirectToReferer: false
+            redirectToReferer: false,
+            loading: false
         }
     }
 
@@ -27,6 +28,7 @@ class Signin extends Component {
 
     clickSubmit = event => {
         event.preventDefault();
+        this.setState({ loading: true })
         const { email, password } = this.state;
         const user = {
             email,
@@ -35,7 +37,7 @@ class Signin extends Component {
         //console.log(user);
         this.signin(user)
             .then(data => {
-                if (data.error) this.setState({ error: data.error })
+                if (data.error) this.setState({ error: data.error, loading: false })
                 else {
                     //authenticate user and redirect
                     this.authenticate(data, () => {
@@ -81,7 +83,7 @@ class Signin extends Component {
 
 
     render() {
-        const { email, password, error, redirectToReferer } = this.state;
+        const { email, password, error, redirectToReferer, loading } = this.state;
 
         if (redirectToReferer) {
             return <Redirect to="/" />
@@ -97,9 +99,18 @@ class Signin extends Component {
                 }}>
                     {error}
                 </div>
+
+                {
+                    loading ? (<div className="jumbotron text-center">
+                        <h2>Loading...</h2></div>)
+                        : ("")
+                }
+
                 {this.singInForm(email, password)}
 
             </div>
+
+
 
         );
     }
