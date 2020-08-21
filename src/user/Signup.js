f03b483966db7238202ccc1500ae27b778ca3cee
Claudio Santos das Virgens <csantos@MB-Pro-de-CSV.local>
2020-08-21 11:25:15 -0300

    first commit Frontend signup

---
+++ src/user/Signup.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            open: false
+        }
+    }
+
+    handleChange = (name) => (event) => {
+        this.setState({ error: "" })
+        this.setState({ [name]: event.target.value });
+    }
+
+    clickSubmit = event => {
+        event.preventDefault();
+        const { name, email, password } = this.state;
+        const user = {
+            name,
+            email,
+            password
+        };
+        //console.log(user);
+        this.signup(user)
+            .then(data => {
+                if (data.error) this.setState({ error: data.error })
+                else this.setState({
+                    name: "",
+                    email: "",
+                    password: "",
+                    error: "",
+                    open: true
+                });
+            })
+
+    };
+
+    signup = user => {
+
+        return fetch("http://localhost:8094/signup", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        }).then(response => {
+            return response.json()
+        })
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        const { name, email, password, error, open } = this.state;
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">Signup</h2>
+
+                <div className="alert alert-primary" style={{
+                    display: error ? "" : "none"
+                }}>
+                    {error}
+                </div>
+
+                <div className="alert alert-info" style={{
+                    display: open ? "" : "none"
+                }}>
+                    New account is sucessfully created. Please Sign In.
+                </div>
+
+                <form>
+                    <div className="form-group">
+                        <label className="text-muted">Name</label>
+                        <input onChange={this.handleChange("name")} type="text" className="form-control"
+                            value={name} />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Email</label>
+                        <input onChange={this.handleChange("email")} type="email" className="form-control"
+                            value={email} />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Password</label>
+                        <input onChange={this.handleChange("password")} type="password" className="form-control"
+                            value={password} />
+                    </div>
+                    <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">Submit</button>
+
+                </form>
+
+            </div>
+
+        );
+    }
+
+}
+
+export default Signup;
