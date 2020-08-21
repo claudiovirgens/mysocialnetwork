63597ff740df004ea7b5cb296308e2507313e2b3
Claudio Santos das Virgens <csantos@MB-Pro-de-CSV.local>
2020-08-21 18:38:42 -0300

    adding user signout

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,10 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const isActive = (history, path) => {
     if (history.location.pathname === path) return { color: "#ff9900" }
     else return { color: "#ffffff" }
-}
+};
+
+export const signout = (next) => {
+    if (typeof window !== "undefined") localStorage.removeItem("jwt")
+    next()
+    return fetch("http://localhost:8094/signout", {
+        method: "GET"
+    })
+        .then(response => {
+            console.log('singout', response)
+            return response.json()
+        }).catch(err => console.log(err))
+};
+
 
 const Menu = ({ history }) => (
     <div>
@@ -18,8 +32,11 @@ const Menu = ({ history }) => (
             <li className="nav-item">
                 <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Sign Up</Link>
             </li>
+            <li className="nav-item">
+                <a className="nav-link" style={(isActive(history, "/signup"), { cursor: "pointer", color: "#fff" })} onClick={() => signout(() => history.push('/'))} >Sign Out</a>
+            </li>
         </ul>
-    </div>
+    </div >
 )
 
 export default withRouter(Menu);
